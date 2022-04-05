175cf2116c62cd53e5aec43ef41bb010fbf59c28
RomanyukRV <rvregi@gmail.com>
2022-04-05 21:17:13 +0900

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -24,17 +26,34 @@ public class Main {
                         .build())
                 .build();
 
-        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=hc0CfwoM6Kc7gc22dyHv0lWqvFF1DANBIB7CX6r1");
 
         CloseableHttpResponse response = httpClient.execute(request);
 
-        List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Post>>() {
+        Nasa nassa = mapper.readValue(response.getEntity().getContent(), new TypeReference<Nasa>() {
         });
 
-        posts.forEach(System.out::println);
+        //posts.forEach(System.out::println);
 
-        posts.stream()
-                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0))
-        .forEach(System.out::println (value));
+        String url = nassa.getUrl();
+        System.out.println(url);
+
+        HttpGet request2 = new HttpGet(url);
+        CloseableHttpResponse response2 = httpClient.execute(request2);
+
+        String[] urlArray = url.split("/");
+        System.out.println(Arrays.toString(urlArray));
+        String namePicters = urlArray[6];
+        HttpEntity entity = response2.getEntity();
+
+        if (entity != null) {
+
+            try (FileOutputStream fos = new FileOutputStream(namePicters)) {
+
+                entity.writeTo(fos);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
---
+++ src/main/java/Nasa.java
@@ -0,0 +1,79 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Nasa {
+
+    String copyright;
+    String date;
+    String explanation;
+    String hdurl;
+    String mediaType;
+    String serviceVersion;
+    String title;
+    String url;
+
+    public Nasa(
+            @JsonProperty("copyright") String copyright,
+            @JsonProperty("date") String date,
+            @JsonProperty("explanation") String explanation,
+            @JsonProperty("hdurl") String hdurl,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
+            @JsonProperty("title") String title,
+            @JsonProperty("url") String url
+            ) {
+        this.copyright=copyright;
+        this.date=date;
+        this.explanation=explanation;
+        this.hdurl=hdurl;
+        this.mediaType=mediaType;
+        this.serviceVersion=serviceVersion;
+        this.title=title;
+        this.url=url;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public String geterviceVersion() {
+        return serviceVersion;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "Nasa{" +
+                "copyright='" + copyright + '\'' +
+                ", date='" + date + '\'' +
+                ", explanation='" + explanation + '\'' +
+                ", hdurl='" + hdurl + '\'' +
+                ", mediaType='" + mediaType + '\'' +
+                ", serviceVersion='" + serviceVersion + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
